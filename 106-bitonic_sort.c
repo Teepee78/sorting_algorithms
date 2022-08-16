7b8b37c576becd4182ee365b558f241c7cae39de
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-08-16 22:49:15 +0100

    Update

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -28,12 +28,17 @@ void Bitonic_Sort(int *array, size_t size, size_t low, size_t count, int directi
 	if (count < 2)
 		return;
 	k = count / 2;
-	printf("Merging [%ld/%ld] (UP)\n", count, size);
+	if (direction == 1)
+		printf("Merging [%ld/%ld] (UP)\n", count, size);
+	else
+		printf("Merging [%ld/%ld] (DOWN)\n", count, size);
 	print_array(&array[low], count);
 
 	Bitonic_Sort(array, size, low, k, 1);
 	Bitonic_Sort(array, size, low + k, k, 0);
 	bitonic_merge(array, size, low, count, direction);
+	printf("Result [%ld/%ld] (UP)\n", count, size);
+	print_array(&array[low], count);
 }
 
 /**
@@ -59,11 +64,7 @@ void bitonic_merge(int *array, size_t size, size_t low, size_t count, int direct
 		for (i = low; i < low + k; i++)
 		{
 			if (array[i] > array[i + k])
-			{
-				printf("Result [%ld/%ld] (UP)\n", count, size);
 				swap(array, i, i + k, size);
-				print_array(&array[low], count);
-			}
 		}
 	}
 	else
@@ -71,11 +72,7 @@ void bitonic_merge(int *array, size_t size, size_t low, size_t count, int direct
 		for (i = low; i < low + k; i++)
 		{
 			if (array[i] < array[i + k])
-			{
-				printf("Result [%ld/%ld] (DOWN)\n", count, size);
 				swap(array, i, i + k, size);
-				print_array(&array[low], count);
-			}
 		}
 	}
 	bitonic_merge(array, size, low, k, direction);
